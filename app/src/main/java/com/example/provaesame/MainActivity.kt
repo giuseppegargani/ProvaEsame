4e646b5b8891ba0fccc34768ffb8798c8dd9f6b6
giuseppegargani <51164844+giuseppegargani@users.noreply.github.com>
2021-06-03 15:52:55 +0200

    Prima Versione di prova

---
+++ app/src/main/java/com/example/provaesame/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.provaesame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+1 - Prova Controllo di versionamento
+    https://getstream.io/blog/use-github-android-studio/
+    istruzioni principali: https://betterprogramming.pub/how-to-use-git-in-android-studio-part-1-a8a554006aad  interessante!!
+    come creare un Token: https://stackoverflow.com/questions/64869735/cant-log-in-to-github-on-android-studio
+2 - Creazione di un DB
+3 - RecyclerView con multiframmento e inserimento dati
+
+ */
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
