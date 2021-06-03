9d3faa3dc5d81a09367da7d161b8039697d7a266
giuseppegargani <51164844+giuseppegargani@users.noreply.github.com>
2021-06-03 16:36:54 +0200

    Terza versione di prova

--- app/src/main/java/com/example/provaesame/MainActivity.kt
+++ app/src/main/java/com/example/provaesame/MainActivity.kt
@@ -8,8 +8,11 @@ import android.os.Bundle
     https://getstream.io/blog/use-github-android-studio/
     istruzioni principali: https://betterprogramming.pub/how-to-use-git-in-android-studio-part-1-a8a554006aad  interessante!!
     come creare un Token: https://stackoverflow.com/questions/64869735/cant-log-in-to-github-on-android-studio
+    reset to last commit: https://stackoverflow.com/questions/29616173/how-to-reset-to-my-last-commit-in-android-studio/29616255
 2 - Creazione di un DB
 3 - RecyclerView con multiframmento e inserimento dati
+4 - impostare test per controllare cose prima di fare push su github
+
 
  */
 
